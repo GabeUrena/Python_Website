03464d251c20351c94ed1a5781335c4d2ed7bb84
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-11-15 16:10:47 -0500

    Added product and accessed admin login

--- mysite/products/models.py
+++ mysite/products/models.py
@@ -8,3 +8,7 @@ class Product(models.Model):
     image_url = models.CharField(max_length = 2083)
     weight = models.FloatField()
     
+class Offer(models.Model):
+    code = models.CharField(max_length=25)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
