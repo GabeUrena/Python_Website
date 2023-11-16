8ae8d58d5fc77fa290ba4d31386a2057853fb548
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2023-11-16 16:45:19 -0500

    Creating index and base html for products page

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    #Products.objects.all() returns all products in DB
+    #Products.objects.filter() filters out products based on a condition "out of stock or that cost more than $100"
+    #Products.objects.get() getting a single product
+    #Products.objects.save() inserting a new product or updating an exisitng one
+    
+    products = Product.objects.all()
+    return render(request, "index.html",{"products": products})
 
 def new(request):
     return HttpResponse("New products! Just in today!")
